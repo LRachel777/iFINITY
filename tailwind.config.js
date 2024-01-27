c8a88d38339ca8e5f1d14df2285b1a87020c1540
LRachel777 <111403837+LRachel777@users.noreply.github.com>
2024-01-27 19:08:41 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+   screens: {
+    sm: '640px',
+
+    md: '768px',
+
+    lg: '1024px'  
+
+
+   }
+  },
+  plugins: [],
+}
+
